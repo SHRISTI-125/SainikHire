aaf20b9e31f52289537815f0dd37ac5036be4fc0
Shristi Kumari <136554443+SHRISTI-125@users.noreply.github.com>
2025-07-09 13:58:15 +0530

    Update recommend_job_logic.py

--- Searching and Recommendation Backend/recommend_job_logic.py
+++ Searching and Recommendation Backend/recommend_job_logic.py
@@ -1,25 +1,47 @@
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
+#cleaning and optimizing job and user text
+def clean_optimize(prompt):
+    return prompt.lower().replace('\n', ' ').strip()
+
+#embedding skills
+def skill_embedding(skills):
+    add = ', '.join(skills)
+    return f"These are the skills {add} required for current role"
+
+#20% weightage to keywords
+def keyword_overlap_boost(user_skills, job_skills):
+    match_count = sum(skill.lower() in job_skills.lower() for skill in user_skills)
+    return match_count / len(user_skills) if user_skills else 0
+
 # recommendation logic
 def recommend_jobs_logic(user_input,db, model, collection, top_k=6):
     if '@gmail.com' in user_input:
         user = db['users'].find_one({'email': user_input})
-    else:
-        user = db['users'].find_one({'password': user_input})
+    #else:
+    #    user = db['users'].find_one({'password': user_input})
     try:
         if not user or 'skills' not in user:
             return []
-        user_skill = ' '.join(user['skills'])
+        user_skill_list = user['skills']
+        user_skill_all = ' '.join(user_skill_list)
+        user_skill = clean_optimize(user_skill_all)
         user_emb = model.encode(user_skill).reshape(1, -1)
     except Exception as e:
         return []
 
     results = []
-    for job in collection.find({"skills_embedding": {"$exists": True}}):
+    for job in collection.find({"skills": {"$exists": True}}):
         try:
-            job_emb = np.array(job["skills_embedding"]).reshape(1, -1)
+            job_skill_all = skill_embedding(job["skills"])
+            job_optimize = clean_optimize(job_skill_all)#converting into lower case->because sentence transformer will work well.
+            job_emb = model.encode(job_optimize).reshape(1, -1)
             score = cosine_similarity(user_emb, job_emb)[0][0]
+
+            keyword_boost = keyword_overlap_boost(user_skill_list, ' '.join(job["skills"]))
+            final_score = score + keyword_boost * 0.2
+            
             results.append({
                 "_id": str(job["_id"]),
                 "title": job.get("title", ""),
@@ -32,7 +54,7 @@ def recommend_jobs_logic(user_input,db, model, collection, top_k=6):
                 "description": job.get("description", ""),
                 "skills": job.get("skills", ""),
                 "apply_link": job.get("apply_link", ""),
-                "score": round(float(score), 4)
+                "score": round(float(final_score), 4) #after giving 20% weight to keyword, beacuse it increase similarity%
             })
         except Exception as e:
             print("Skip job due to error:", e)
