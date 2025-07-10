20da089142561f2cf8bd35018da76523bb203c70
Shristi Kumari <136554443+SHRISTI-125@users.noreply.github.com>
2025-07-10 13:56:50 +0530

    Update email_sending.py

--- Searching and Recommendation Backend/email_sending.py
+++ Searching and Recommendation Backend/email_sending.py
@@ -6,6 +6,7 @@ from pymongo import MongoClient
 from datetime import datetime, timedelta
 import time
 import threading
+from sklearn.metrics.pairwise import cosine_similarity
 
 password=password()
 # Email 
@@ -23,39 +24,81 @@ def send_job_reminders():
     today = datetime.now().date()
     time_left = today + timedelta(days=5) # job ending in 5 days
 
-    upcoming_jobs = list(jobs_col.find({
+    ending_jobs = list(jobs_col.find({
         "last_date": {
             "$gte": today.strftime('%Y-%m-%d'),
             "$lte": time_left.strftime('%Y-%m-%d')
         }
     }))
 
-    if not upcoming_jobs:
+    if not ending_jobs:
         print("No upcoming jobs found.")
         return
+    userupdating_recommendations = {}
 
     # summary
     job_lines = []
-    for job in upcoming_jobs:
-        job_lines.append(f"{job['title']} - Last Date: {job['last_date']}\nLink: {job.get('apply_link', 'http://localhost:5173/')}\n")
+    job_skills = []
+    for job in ending_jobs:
+        job_title = job['title']
+        job_link = job.get('apply_link', 'http://localhost:5173/')
+        job_last_date = job['last_date']
+        job_skill_text = job.get('skills', []) 
+
+        job_embedding = model.encode(job_skill_text, convert_to_tensor=False) #converting job skills into embedding
+
+        matching_users = []
+
+        for user in users_col.find():
+            user_email = user.get('email')
+            user_skills = user.get('skills', [])
+
+            if not user_email or not user_skills:
+                continue
+
+            user_embedding = model.encode(user_skills, convert_to_tensor=False) #converting user skills into embedding
+            similarity = cosine_similarity([user_embedding], [job_embedding])[0][0] #finding similarity
+
+            if similarity >= 0.4:  #if more similar then; take user's email with message
+                matching_users.append(user_email)
+                if user_email not in userupdating_recommendations:
+                    userupdating_recommendations[user_email] = []
+                userupdating_recommendations[user_email].append(f"{job_title} (Last Date: {job_last_date})\nApply: {job_link}\n")
+
+
+        if not matching_users:
+            print(f"No matching users for job: {job_title}")
+            continue
+            
 
     total_job = "\n".join(job_lines)
 
     subject = "Upcoming Job Deadlines within Next 5 Days"
-    body = f"""Hi there,
-    Here are some jobs whose last dates are coming soon, apply as soon as possible
+#body = f"""Hi there,
+    #Here are some jobs whose last dates are coming soon, apply as soon as possible
     
-    {total_job}
+    #{job_title}
+    #This job is ending at {job_last_date}.
+    #Apply Here: {job_link}
     
-    Regards,
-    SainikHire
-    """
+    #Regards,
+    #SainikHire
+    #"""
 
-    user_emails = [user['email'] for user in users_col.find()]
 
     with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp:
         smtp.login(SENDER_EMAIL, APP_PASSWORD)
-        for email in user_emails:
+        for email,job_list in userupdating_recommendations.items():
+            body = f"""Hi There,
+            The following jobs matched your skills and are ending in next 5 days.
+            
+            {"\n".join(job_list)}
+            
+            Apply now!
+
+            Regards,
+            SainikHire
+            """
             msg = EmailMessage()
             msg['Subject'] = subject
             msg['From'] = SENDER_EMAIL
